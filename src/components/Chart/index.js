948001addb5d3af8a5722d990a6bcd0f4523a1a2
terry623 <terry92516@gmail.com>
2018-12-16 09:28:40 +0800

    remove window scroller

--- src/components/Chart/index.js
+++ src/components/Chart/index.js
@@ -1,8 +1,6 @@
 import InfiniteLoader from 'react-virtualized/dist/commonjs/InfiniteLoader';
-import React, { Component, Fragment } from 'react';
+import React, { Component } from 'react';
 import VList from 'react-virtualized/dist/commonjs/List';
-import WindowScroller from 'react-virtualized/dist/commonjs/WindowScroller';
-import { message } from 'antd';
 
 import './index.css';
 
@@ -17,10 +15,7 @@ import Cell from './Cell';
 class Chart extends Component {
   state = {
     data: [],
-    loading: false,
   };
-
-  loadedRowsMap = {};
 
   async componentDidMount() {
     const results = await getForestRankingDataFromServer({
@@ -30,39 +25,22 @@ class Chart extends Component {
     this.setState({ data: results });
   }
 
-  componentDidUpdate() {
-    if (this.state.loading) {
-      const hide = message.loading('Action in progress..', 0);
-      setTimeout(hide, 1500);
-    }
-  }
-
   handleInfiniteOnLoad = async ({ startIndex, stopIndex }) => {
     let { data } = this.state;
-    this.setState({ loading: true });
-
     console.log(`Start: ${startIndex}, Stop ${stopIndex}`);
 
-    for (let i = startIndex; i <= stopIndex; i++) {
-      // 1 means loading
-      this.loadedRowsMap[i] = 1;
-    }
-
-    if (data.length >= maximumNumber) {
-      message.warning('Infinite List loaded all');
-      this.setState({ loading: false });
-      return;
-    }
-
     const results = await getForestRankingDataFromServer({
-      lastPosition: data.length,
+      lastPosition: startIndex,
     });
 
     data = data.concat(results);
-    this.setState({ data, loading: false });
+    this.setState({ data });
   };
 
-  isRowLoaded = ({ index }) => !!this.loadedRowsMap[index];
+  isRowLoaded = ({ index }) => {
+    const { data } = this.state;
+    return !!data[index];
+  };
 
   renderItem = ({ index, key, style }) => {
     const { data } = this.state;
@@ -73,58 +51,27 @@ class Chart extends Component {
   render() {
     const { data } = this.state;
 
-    const vlist = ({
-      height,
-      isScrolling,
-      onChildScroll,
-      scrollTop,
-      onRowsRendered,
-    }) => (
-      <VList
-        autoHeight
-        className="main"
-        height={height}
-        isScrolling={isScrolling}
-        onScroll={onChildScroll}
-        overscanRowCount={2}
-        rowCount={data.length}
-        rowHeight={80}
-        rowRenderer={this.renderItem}
-        onRowsRendered={onRowsRendered}
-        scrollTop={scrollTop}
-        width={400}
-      />
-    );
-
-    const infiniteLoader = ({
-      height,
-      isScrolling,
-      onChildScroll,
-      scrollTop,
-    }) => (
+    return (
       <InfiniteLoader
         isRowLoaded={this.isRowLoaded}
         loadMoreRows={this.handleInfiniteOnLoad}
-        rowCount={data.length}
+        rowCount={maximumNumber}
         minimumBatchSize={recordsNumber}
-        threshold={0}
+        threshold={1}
       >
-        {({ onRowsRendered }) =>
-          vlist({
-            height,
-            isScrolling,
-            onChildScroll,
-            scrollTop,
-            onRowsRendered,
-          })
-        }
+        {({ onRowsRendered, registerChild }) => (
+          <VList
+            className="main"
+            height={640}
+            onRowsRendered={onRowsRendered}
+            ref={registerChild}
+            rowCount={data.length}
+            rowHeight={80}
+            rowRenderer={this.renderItem}
+            width={400}
+          />
+        )}
       </InfiniteLoader>
-    );
-
-    return (
-      <Fragment>
-        {data.length > 0 && <WindowScroller>{infiniteLoader}</WindowScroller>}
-      </Fragment>
     );
   }
 }
